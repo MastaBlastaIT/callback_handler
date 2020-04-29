31eff10abf555b14550fd5ecb588ddf74b368ab2
Ilya Tishkov <i.tishkov@gateline.ru>
2020-04-30 01:02:01 +0300

    git added to deploy_changes

---
+++ flask_app/__init__.py
@@ -0,0 +1,24 @@
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, request
+import os, git
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello from Flask!"
+
+
+@app.route("/deploy_changes", methods=["POST"])
+def deploy_changes():
+    if request.method == "POST":
+        repo = git.Repo("/home/mastaGateline/callback_handler")
+        origin = repo.remotes.origin
+        origin.pull()
+        # origin.fetch()
+        # repo.merge_base(repo.heads.master, origin.refs.master)
+        return "Updated PythonAnywhere successfully", 200
+    else:
+        return "Wrong event type", 400
